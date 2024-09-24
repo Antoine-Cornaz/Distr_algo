5fe0fd0c7bc9c035c787d2453cce97bd92f319ca
Antoine Cornaz <antoine.cornaz01@gmail.com>
2024-09-24 14:01:18 +0200

    print output parser

--- template_java/src/main/java/cs451/Main.java
+++ template_java/src/main/java/cs451/Main.java
@@ -58,6 +58,8 @@ public class Main {
 
         System.out.println("Broadcasting and delivering messages...\n");
 
+        System.out.println("output parser:" + parser.output());
+
         // After a process finishes broadcasting,
         // it waits forever for the delivery of messages.
         while (true) {
